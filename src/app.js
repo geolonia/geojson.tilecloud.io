b0dfce1e950ab402b6e1d3e15a481dcd075ec6e7
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-20 04:06:35 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import 'babel-polyfill'
+import MapboxDraw from '@mapbox/mapbox-gl-draw'
+import styleControl from './lib/styleControl'
+
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import './style.scss'
+
+const map = new tilecloud.Map( document.getElementById( 'map' ) )
+
+const draw = new MapboxDraw( {
+  controls: {
+    point: true,
+    line_string: true,
+    polygon: true,
+    trash: true,
+    combine_features: false,
+    uncombine_features: false,
+  },
+} )
+
+map.addControl( draw, 'top-right' );
+map.addControl(new styleControl() );
+
+[ 'draw.create', 'draw.update', 'draw.delete' ].forEach( ( eventType ) => {
+  map.on( eventType, () => {
+    const textArea = document.getElementById( 'geojson' )
+    const geoJson = draw.getAll()
+    textArea.value = JSON.stringify( geoJson, null, '  ' )
+  } )
+} )
+
+const drawSet = () => {
+  if ( document.getElementById( 'geojson' ).value.trim() ) {
+    draw.set( JSON.parse( document.getElementById( 'geojson' ).value ) )
+  }
+}
+
+document.getElementById( 'geojson' ).addEventListener( 'change', drawSet )
+window.addEventListener( 'load', drawSet )
---
+++ src/lib/styleControl.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import 'whatwg-fetch'
+import 'promise-polyfill/src/polyfill'
+
+class styleControl {
+  constructor() {
+
+  }
+
+  onAdd() {
+    this.container = document.createElement('div')
+    this.container.className = 'mapboxgl-ctrl'
+
+    return this.container;
+  }
+
+  onRemove() {
+    this.container.parentNode.removeChild( this.container )
+  }
+
+  getDefaultPosition() {
+    return 'top-left'
+  }
+}
+
+export default styleControl;
